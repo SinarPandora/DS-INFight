296720a97e3f6c0fe8ff6a9c6407ca8de577a885
Sinar Pandora <SinarPandora@outlook.com>
2024-08-29 18:15:08 +0800

    主页文件名更新

--- src/renderer/router/index.ts
+++ src/renderer/router/index.ts
@@ -1,21 +1,21 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Dashboard from '../modules/dashboard/Dashboard.vue'
-import Party from '../modules/party/Party.vue'
-import Fight from '../modules/fight/Fight.vue'
-import Explorer from '../modules/explorer/Explorer.vue'
-import Training from '../modules/training/Training.vue'
-import Synthesize from '../modules/synthesize/Synthesize.vue'
+import PartyOverview from '../modules/party/PartyOverview.vue'
+import FightOverview from '../modules/fight/FightOverview.vue'
+import ExplorerOverview from '../modules/explorer/ExplorerOverview.vue'
+import TrainingOverview from '../modules/training/Training.vue'
+import SynthesizeOverview from '../modules/synthesize/SynthesizeOverview.vue'
 import Settings from '../modules/settings/Settings.vue'
 
 export const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     { path: '/', name: 'dashboard', component: Dashboard },
-    { path: '/party', name: 'party', component: Party },
-    { path: '/fight', name: 'fight', component: Fight },
-    { path: '/explorer', name: 'explorer', component: Explorer },
-    { path: '/training', name: 'training', component: Training },
-    { path: '/synthesize', name: 'synthesize', component: Synthesize },
+    { path: '/party', name: 'party', component: PartyOverview },
+    { path: '/fight', name: 'fight', component: FightOverview },
+    { path: '/explorer', name: 'explorer', component: ExplorerOverview },
+    { path: '/training', name: 'training', component: TrainingOverview },
+    { path: '/synthesize', name: 'synthesize', component: SynthesizeOverview },
     { path: '/settings', name: 'settings', component: Settings },
     { path: '/:catchAll(.*)*', name: '404', redirect: '/' }
   ]
